094d4757656eaf54e0b34e844791699ca722a01f
ShabanaParveen07 <skparveen.it@gmail.com>
2024-08-10 13:17:54 +0530

    2nd day progs

---
+++ src/Ex_10082024/Lab002.py
@@ -0,0 +1,5 @@
+a = 20, 30, 40, 50
+print(max(a))
+
+b= 30,60,100, "Shabana"
+print(max(b))  # Sting doesn't support max function
---
+++ src/Ex_10082024/Lab003.py
@@ -0,0 +1,9 @@
+print("Hello!" "Shabana Parveen", end='\n')
+
+_123= "Python"
+print(_123)
+
+x=2
+y=5
+sum=x+y
+print(sum)
